a59fc137ba3491debc0ec8b482635f7a73c6784b
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2021-08-27 01:33:51 -0300

    Adds build-podman-arm

---
+++ build-podman-arm.sh
@@ -0,0 +1,5 @@
+podman build -f ./Guetta/Dockerfile -t redbaty/homelab:guetta-arm . &
+podman build -f ./Guetta.Player/Dockerfile -t redbaty/homelab:guetta-player-arm . &
+
+podman push redbaty/homelab:guetta-arm
+podman push redbaty/homelab:guetta-player-arm
