930ddf2a215009417d9627ac29f6eec09aeac7ff
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2021-08-27 01:32:38 -0300

    Adiciona build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+docker build -f ./Guetta/Dockerfile -t redbaty/homelab:guetta . &
+docker build -f ./Guetta.Player/Dockerfile -t redbaty/homelab:guetta-player . &
+
+docker push redbaty/homelab:guetta
+docker push redbaty/homelab:guetta-player
