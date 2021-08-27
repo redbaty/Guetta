8a2344043d845333d1daa582937e75a1c1eb2c55
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2021-08-27 01:40:12 -0300

    Wait podman builds

--- build-podman-arm.sh
+++ build-podman-arm.sh
@@ -1,5 +1,6 @@
 podman build -f ./Guetta/Dockerfile -t redbaty/homelab:guetta-arm . &
 podman build -f ./Guetta.Player/Dockerfile -t redbaty/homelab:guetta-player-arm . &
+wait
 
 podman push redbaty/homelab:guetta-arm
 podman push redbaty/homelab:guetta-player-arm
--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 docker build -f ./Guetta/Dockerfile -t redbaty/homelab:guetta . &
 docker build -f ./Guetta.Player/Dockerfile -t redbaty/homelab:guetta-player . &
+wait
 
 docker push redbaty/homelab:guetta
 docker push redbaty/homelab:guetta-player
