93ae3a0ece3fca2288b98459854a7b0abacc0717
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2021-09-07 23:33:16 -0300

    Updates build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,10 @@
-docker build -f .\Guetta\Dockerfile -t redbaty/homelab:guetta .
-docker build -f .\Guetta.Player\Dockerfile -t redbaty/homelab:guetta-player .
+docker build -f .\Guetta\Dockerfile -t redbaty/guetta:latest .
+docker build -f .\Guetta.Queue\Dockerfile -t redbaty/guetta-queue:latest .
 
-docker push redbaty/homelab:guetta
-docker push redbaty/homelab:guetta-player
+Set-Location guetta-player-js
+docker build -t redbaty/guetta-player-js:latest .
+Set-Location ..
+
+docker push redbaty/guetta:latest
+docker push redbaty/guetta-queue:latest
+docker push redbaty/guetta-player-js:latest
